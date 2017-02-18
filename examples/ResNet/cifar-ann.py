fix grad weight logging; change sum_rand_ratio normalization

examples/ResNet/cifar-ann.py
@@ -203,12 +203,9 @@ class Model(ModelDesc):
                         if WM_WEIGHT or TRACK_GRADIENTS:
                             gs = tf.gradients(c, tf.trainable_variables())
                             reward =  tf.add_n([tf.nn.l2_loss(g) for g in gs if g is not None], 
-                                               name='l2_grad_{}'.format(anytime_idx))
+                                               name='l2_grad_{}'.format(anytime_idx-1))
                             add_moving_summary(reward)
-                        if RAND_WEIGHT or EXP3_WEIGHT: 
-                            cost += (1.0 / (SUM_RAND_RATIO + 1)) * (SUM_RAND_RATIO * cost_weight + add_weight) * c
-                        else: 
-                            cost += cost_weight * c
+                        cost += (cost_weight + add_weight / SUM_RAND_RATIO) * c
                         # Regularize weights from FC layers. Should use 
                         # regularize_cost to get the weights using variable names
                         wd_cost += cost_weight * wd_w * tf.nn.l2_loss(var_list[2*ci])