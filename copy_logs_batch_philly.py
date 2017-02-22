1a8bfe4cd3c01864e54b5dfe59aea8a5fc8607d9
Debadeepta Dey <dedey@microsoft.com>
2017-02-22 13:57:31 -0800

    Minor.

--- copy_logs_batch_philly.py
+++ copy_logs_batch_philly.py
@@ -50,7 +50,7 @@ def copy_passed_logs_from_json(json_data=None):
 				philly_scratch_dir = job['scratch']
 
 				# The name of the folder on the local machine to put logs in
-				local_dir = os.path.join('/home/dedey/DATADRIVE1/catch_all', job_name.split('.')[0])
+				local_dir = os.path.join('/home/dedey/DATADRIVE2/ann_models_logs', job_name.split('.')[0])
 
 				# Call the bash script with the arguments
 				print 'Going to copy: ' + philly_scratch_dir + ' to ' + local_dir 
