97bf44f783c37feb9b861de8079080d976a702a3
hanzhang <hanzhang@andrew.cmu.edu>
2017-02-24 23:09:09 -0500

    fix exp3 and rwm

--- tensorpack/models/online.py
+++ tensorpack/models/online.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 
+from ..tfutils.summary import *
 from ..tfutils.tower import get_current_tower_context
 from ..utils import logger
 
@@ -9,40 +10,53 @@ __all__ = ['Exp3', 'HalfEndHalfExp3', 'RandSelect', 'RWM']
 class Exp3(object):
     def __init__(self, name, K, gamma):
         with tf.variable_scope(name) as scope:
+            ctx = get_current_tower_context()
+            self.inactive = ctx.is_training is not None and not ctx.is_training
+            if self.inactive:
+                return
             self.name = name
             self.K = K
             self.gamma = gamma
             self.w = tf.get_variable("w", [1, self.K], 
                 initializer=tf.constant_initializer(1.0/self.K), trainable=False)
-            ctx = get_current_tower_context()
-            self.inactive = ctx.is_training is not None and not ctx.is_training
+            for k in range(self.K):
+                add_moving_summary(self.w[0][k])
 
     def sample(self):
         if self.inactive:
             return -1, 0.0
 
-        with tf.variable_scope(self.name):
-            probs = (1.0 - self.gamma) * self.w + self.gamma / self.K
+        with tf.variable_scope(self.name) as scope:
+            scope.reuse_variables()
+            w = tf.get_variable("w")
+            probs = (1.0 - self.gamma) * w + self.gamma / self.K
+            #w = tf.Print(w, [w], "sample")
             idx = tf.cast(tf.multinomial(tf.log(probs), 1)[0][0], tf.int32)
-            p_idx = probs[idx]
+            p_idx = probs[0][idx]
             return idx, p_idx
 
     def update(self, idx, p_idx, reward):
         if self.inactive:
             return tf.zeros(())
             
-        with tf.variable_scope(self.name):
-            reward = reward / p_idx
-            r_vec = tf.one_hot(idx, self.K, tf.exp(self.gamma * reward /self.K), 1.0)
-            self.w = tf.multiply(self.w, r_vec)
-            w_sum = tf.reduce_sum(self.w)
-            self.w = self.w / w_sum
-            tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, tf.identity(self.w))
+        with tf.variable_scope(self.name) as scope:
+            with tf.control_dependencies([idx, p_idx, reward]):
+                scope.reuse_variables()
+                w = tf.get_variable("w")
+                r_vec = tf.reshape(tf.one_hot(idx, self.K, 
+                    tf.exp(self.gamma * reward / (p_idx * self.K)), 1.0), [1,self.K])
+                un_w = tf.multiply(w, r_vec)
+                op = tf.assign(w, un_w / tf.reduce_sum(un_w)) 
 
-        return tf.zeros(())
+                #op = tf.assign(w, tf.reshape(tf.one_hot(idx, self.K, 1., 0.), [1,self.K]))
+                #op = tf.Print(op, [op], "update")
+                with tf.control_dependencies([op]):
+                    ret = tf.zeros(())
+            return ret
+        #tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, tf.identity(self.w))
 
 class RWM(object):
-    def __init__(self, name, K, gamma, weights):
+    def __init__(self, name, K, gamma):
         with tf.variable_scope(name) as scope:
             ctx = get_current_tower_context()
             self.inactive = ctx.is_training is not None and not ctx.is_training
@@ -51,33 +65,44 @@ class RWM(object):
             self.name = name
             self.K = K
             self.gamma = gamma
-            self.w = tf.get_variable("w", [1, self.K], trainable=False) 
-            self.default_w = weights.reshape((1,self.K))/np.sum(weights)
-            self.w.assign(self.default_w)
+            self.default_w = np.ones((1,self.K),dtype=np.float32) / self.K
+            self.w = tf.get_variable("w", [1, self.K], 
+                initializer=tf.constant_initializer(self.default_w), trainable=False) 
+            for k in range(self.K):
+                add_moving_summary(self.w[0][k])
     
     def sample(self):
         if self.inactive:
             return -1, 0.0
 
-        with tf.variable_scope(self.name):
-            probs = (1.0 - self.gamma) * self.w + self.gamma * self.default_w
-            idx = tf.cast(tf.multinomial(tf.log(probs), 1)[0][0], tf.int32)
-            p_idx = probs[idx]
-            return idx, p_idx
+        with tf.variable_scope(self.name) as scope:
+            scope.reuse_variables()
+            w = tf.get_variable("w")
+            op = tf.assign(w, w / tf.reduce_sum(w))
+            #op = tf.Print(op, [op], "sample")
+            with tf.control_dependencies([op]):
+                probs = (1.0 - self.gamma) * w + self.gamma * self.default_w
+                idx = tf.cast(tf.multinomial(tf.log(probs), 1)[0][0], tf.int32)
+                p_idx = probs[0][idx]
+                return idx, p_idx
 
     def update(self, idx, reward):
         if self.inactive:
             return tf.zeros(())
             
-        with tf.variable_scope(self.name):
-            reward = reward 
-            r_vec = tf.one_hot(idx, self.K, tf.exp(self.gamma * reward / self.K), 1.0)
-            self.w = tf.multiply(self.w, r_vec)
-            w_sum = tf.reduce_sum(self.w)
-            self.w = self.w / w_sum
-            tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, tf.identity(self.w))
+        with tf.variable_scope(self.name) as scope:
+            with tf.control_dependencies([idx, reward]):
+                scope.reuse_variables()
+                w = tf.get_variable("w")
+                r_vec = tf.reshape(tf.one_hot(idx, self.K, 
+                                              tf.exp(self.gamma * reward / self.K), 1.0),
+                                   [1, self.K])
+                op = tf.assign(w, tf.multiply(w, r_vec))
+                #op = tf.Print(op, [op], "update")
+                with tf.control_dependencies([op]):
+                    ret = tf.zeros(())
+            return ret
 
-        return tf.zeros(())
 
 class RandSelect(object):
     def __init__(self, name, K):
