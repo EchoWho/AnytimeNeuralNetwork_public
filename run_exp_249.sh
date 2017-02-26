a0c3329b72373abf68b8c4289910ad83f8cf7418
hanzhang <hanzhang@andrew.cmu.edu>
2017-02-26 17:41:52 -0500

    experiment batch 7 change

--- run_exp_249.sh
+++ run_exp_249.sh
@@ -10,6 +10,7 @@
 DATA_DIR=NONE
 LOG_DIR=NONE
 CONFIG_DIR=NONE
+MODEL_DIR=NONE
 
 # Parsing command line arguments:
 while [[ $# > 0 ]]
@@ -23,6 +24,7 @@ case $key in
     echo "  -d|--data-dir <path> - directory path to input data (default NONE)"
     echo "  -l|--log-dir <path> - directory path to save the log files (default NONE)"
     echo "  -p|--config-file-dir <path> - directory path to config file directory (default NONE)"
+    echo "  -m|--model-dir <path> - directory path to output model file (default NONE)"
     exit 1
     ;;
     -d|--data-dir)
@@ -32,10 +34,13 @@ case $key in
     -p|--config-file-dir)
     CONFIG_DIR="$2"
     shift # pass argument
+    -m|--model-dir)
+    MODEL_DIR="$2"
+    shift # pass argument
     ;;
     -l|--log-dir)
     LOG_DIR="$2"
- 	;;
+  ;;
     *)
     echo Unkown option $key
     ;;
@@ -47,6 +52,7 @@ done
 echo "DATA_DIR=$DATA_DIR"
 echo "LOG_DIR=$LOG_DIR"
 echo "CONFIG_DIR=$CONFIG_DIR"
+echo "MODEL_DIR=$MODEL_DIR"
 
 # Run training on philly
 
@@ -54,9 +60,10 @@ echo "CONFIG_DIR=$CONFIG_DIR"
 export PYTHONPATH=$PYTHONPATH:$CONFIG_DIR
 
 # Run the actual job
-python $CONFIG_DIR/examples/ResNet/paper_experiments/cifar-ann-paper-exp.py \
+python $CONFIG_DIR/examples/ResNet/cifar-ann.py \
 --data_dir=$DATA_DIR \
 --log_dir=$LOG_DIR \
+--model_dir=$MODEL_DIR \
 -f=6 \
 --num_classes=100 \
 --samloss=0
