5b98cd64a809cc190f11528fc26ce3934464fac7
hanzhang <hanzhang@andrew.cmu.edu>
2017-05-04 12:04:41 -0400

    make imagenet run on higher batchsize

--- run_exp_426.sh
+++ run_exp_426.sh
@@ -68,7 +68,8 @@ python $CONFIG_DIR/examples/ResNet/imagenet-ann-v2.py \
 -f=5 \
 -d=101 \
 --opt_at=-1 \
---batch_size=64 \
+--batch_size=128 \
+--nr_gpu=4 \
 --samloss=0 \
 --last_reward_rate=0.85 \
 --exp_gamma=0.3 \
