8b7859dabb6cbbebcd103036aecc969b2f37510a
hanzhang <hanzhang@andrew.cmu.edu>
2017-06-20 00:17:24 -0400

    (log)dense in the new framework

---
+++ examples/AnytimeNetwork/svhn-dense-ann.py
@@ -0,0 +1,103 @@
+import numpy as np
+import argparse
+import os, sys, datetime
+
+import tensorflow as tf
+from tensorpack import *
+from tensorpack.tfutils.symbolic_functions import *
+from tensorpack.tfutils.summary import *
+from tensorpack.utils import logger, utils, fs
+
+from tensorpack.network_models import anytime_network
+from tensorpack.network_models.anytime_network import AnytimeDensenet
+
+INPUT_SIZE=32
+args=None
+
+def get_data(train_or_test):
+    isTrain = train_or_test == 'train'
+    pp_mean = dataset.SVHNDigit.get_per_pixel_mean()
+    if isTrain:
+        d1 = dataset.SVHNDigit('train')
+        d2 = dataset.SVHNDigit('extra')
+        ds = RandomMixData([d1, d2])
+    else:
+        ds = dataset.SVHNDigit('test')
+
+    if isTrain:
+        augmentors = [
+            imgaug.CenterPaste((40, 40)),
+            imgaug.Brightness(10),
+            imgaug.Contrast((0.8, 1.2)),
+            imgaug.GaussianDeform(  # this is slow. without it, can only reach 1.9% error
+                [(0.2, 0.2), (0.2, 0.8), (0.8, 0.8), (0.8, 0.2)],
+                (40, 40), 0.2, 3),
+            imgaug.RandomCrop((32, 32)),
+            imgaug.MapImage(lambda x: (x - pp_mean)/128.0),
+        ]
+    else:
+        augmentors = [
+            imgaug.MapImage(lambda x: (x - pp_mean)/128.0)
+        ]
+    ds = AugmentImageComponent(ds, augmentors)
+    ds = BatchData(ds, args.batch_size, remainder=not isTrain)
+    if isTrain:
+        ds = PrefetchData(ds, 5, 5)
+    return ds
+
+def get_config():
+    # prepare dataset
+    dataset_train = get_data('train')
+    steps_per_epoch = dataset_train.size()
+    dataset_test = get_data('test')
+
+    # Specify model
+    model=AnytimeDensenet(INPUT_SIZE, args)
+    classification_cbs = model.compute_classification_callbacks()
+    loss_select_cbs = model.compute_loss_select_callbacks()
+
+    lr_schedule = [(1, 0.1), (20, 0.01), (30, 0.001), (45, 0.0001)]
+    return TrainConfig(
+        dataflow=dataset_train,
+        callbacks=[
+            ModelSaver(checkpoint_dir=args.model_dir),
+            InferenceRunner(dataset_test,
+                            [ScalarStats('cost')] + classification_cbs),
+            ScheduledHyperParamSetter('learning_rate', lr_schedule)
+        ] + loss_select_cbs,
+        model=model,
+        steps_per_epoch=steps_per_epoch,
+        max_epoch=60,
+    )
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--log_dir', help='log_dir position',
+                        type=str, default=None)
+    parser.add_argument('--data_dir', help='data_dir position',
+                        type=str, default=None)
+    parser.add_argument('--model_dir', help='model_dir position',
+                        type=str, default=None)
+    parser.add_argument('--gpu', help='comma separated list of GPU(s) to use.')
+    parser.add_argument('--load', help='load model')
+    anytime_network.parser_add_densenet_arguments(parser)
+    args = parser.parse_args()
+
+    assert args.num_classes == 10, args.num_classes
+
+    logger.info("Arguments: {}".format(args))
+    logger.info("TF version: {}".format(tf.__version__))
+
+    if args.gpu:
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
+    logger.set_log_root(log_root=args.log_dir)
+    logger.auto_set_dir()
+    fs.set_dataset_path(path=args.data_dir, auto_download=False)
+
+    config = get_config()
+    if args.load and os.path.exists(arg.load):
+        config.session_init = SaverRestore(args.load)
+    if args.gpu:
+        config.nr_tower = len(args.gpu.split(','))
+    SyncMultiGPUTrainer(config).train()
