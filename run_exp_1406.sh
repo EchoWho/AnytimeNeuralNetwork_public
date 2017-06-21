ea1648486746a37f804d17fb92fc615ddc5c0698
hanzhang <hanzhang@andrew.cmu.edu>
2017-06-21 03:27:52 -0400

    update experiment 23 with the new framework

--- run_exp_1406.sh
+++ run_exp_1406.sh
@@ -61,16 +61,17 @@ echo "MODEL_DIR=$MODEL_DIR"
 export PYTHONPATH=$PYTHONPATH:$CONFIG_DIR
 
 # Run the actual job
-python $CONFIG_DIR/examples/ResNet/cifar-logdense.py \
+python $CONFIG_DIR/examples/AnytimeNetwork/dense-ann.py \
 --data_dir=$DATA_DIR \
 --log_dir=$LOG_DIR \
 --model_dir=$MODEL_DIR \
+--ds_name=cifar100 \
 -f=2 \
 --opt_at=116 \
--n=52 \
+-n=32 \
 -g=24 \
--s=6 \
---num_classes=100 \
---log_method=2 \
---log_ann_method=0 \
---batch_size=32 \
+-s=3 \
+--dense_select_method=3 \
+--batch_size=64 \
+--samloss=0
+    
