d54e57e70ed282d1fabc0f7261651b8df33e4281
Debadeepta Dey <dedey@microsoft.com>
2017-06-26 16:20:01 -0700

    Minor.

--- launch_job_rest_api.sh
+++ launch_job_rest_api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {1007..1048}
+for i in {1051..1060}
 do
 
 	USERNAME="dedey"
@@ -13,11 +13,11 @@ do
 
 	CMD="https://philly/api/submit?"
 	CMD+="buildId=0000&"
-	CMD+="customDockerName=custom-tf-0-12-python-2-7-ver2&"
+	CMD+="customDockerName=custom-tf-1-1-python-2-7&"
 	CMD+="toolType=cust&"
 	CMD+="clusterId=$CLUSTER&"
 	CMD+="vcId=$VC&"
-	CMD+="configFile=$USERNAME%2FAnytimeNeuralNetwork%2F$JOBSCRIPT&"
+	CMD+="configFile=$USERNAME%2FAnytimeNeuralNetwork_master%2F$JOBSCRIPT&"
 	CMD+="minGPUs=$NUM_GPUS&"
 	CMD+="name=cust-p-$JOBSCRIPT$SPECIAL_NAME!~!~!1&"
 	CMD+="isdebug=false&"
