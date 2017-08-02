e6f0e91a1118460d076f612ec76a44700a479a4e
hanzhang <hanzhang@andrew.cmu.edu>
2017-08-02 11:33:21 -0400

    update create_image_lmdb to be more automatic and portable

--- scripts/create_imagenet_lmdb.py
+++ scripts/create_imagenet_lmdb.py
@@ -2,18 +2,22 @@ import numpy as np
 import os
 from tensorpack import *
 
+meta_dir = '/data2/ILSVRC2012/caffe_meta'
+lmdb_dir = '/data2/ILSVRC2012/lmdb2'
+raw_dir = '/data2/ILSVRC2012/raw'
 
+dir_structure = 'train' #original 
 
 class RawILSVRC12(DataFlow):
-    def __init__(self, split):
-        meta = dataset.ILSVRCMeta('/data2/ILSVRC2012/caffe_meta')
+    def __init__(self, split, shuffle=True):
+        meta = dataset.ILSVRCMeta(meta_dir)
         self.imglist = meta.get_image_list(split)
 # we apply a global shuffling here because later we'll only use local shuffling
-        if split == 'train':
+        if shuffle:
             np.random.shuffle(self.imglist)
-        self.dir = os.path.join('/data2/ILSVRC2012/raw', split)
+        self.dir = os.path.join(raw_dir, split)
         self.name = split
-        self.dir_structure = 'train'
+        self.dir_structure = dir_structure
         self.synset = meta.get_synset_1000()
 
     def get_data(self):
@@ -32,7 +36,10 @@ class RawILSVRC12(DataFlow):
         return len(self.imglist)
 
 
-split='val'
-ds0 = RawILSVRC12(split)
-ds1 = PrefetchDataZMQ(ds0, nr_proc=1)
-dftools.dump_dataflow_to_lmdb(ds1, '/data2/ILSVRC2012/ilsvrc2012_{}.lmdb'.format(split))
+for split in ['train', 'val']:
+    do_shuffle = split == 'train'
+    ds0 = RawILSVRC12(split, shuffle=do_shuffle)
+    np.savez(os.path.join(lmdb_dir, 'ilsvrc2012_{}_imglist.npz'.format(split)), imglist=ds0.imglist)
+    ds1 = PrefetchDataZMQ(ds0, nr_proc=1)
+    dftools.dump_dataflow_to_lmdb(ds1, 
+        os.path.join(lmdb_dir, 'ilsvrc2012_{}.lmdb'.format(split)))
