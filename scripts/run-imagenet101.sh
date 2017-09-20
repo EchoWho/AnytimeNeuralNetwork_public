2ae0096892ec16fa9440dce0f45696533049e779
hanzhang <hanzhang@andrew.cmu.edu>
2017-09-20 19:07:57 -0400

    sample script for imagenet

---
+++ scripts/run-imagenet101.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=0,3
+DATA_DIR=${GLOBAL_DATA_DIR}
+LOG_DIR=${GLOBAL_LOG_DIR}/imagenet-ann101/
+MODEL_DIR=${GLOBAL_MODEL_DIR}/imagenet_model/ann101/
+CONFIG_DIR=/home/hanzhang/code/tensorpack/
+
+# Run the actual job
+python $CONFIG_DIR/examples/AnytimeNetwork/imagenet-ann.py \
+--data_dir=$DATA_DIR \
+--log_dir=$LOG_DIR \
+--model_dir=$MODEL_DIR \
+-f=5 \
+--opt_at=-1 \
+-d=101 \
+--nr_gpu=2 \
+--batch_size=64 \
+--samloss=6 \
+-c=64 \
+-s=3 \
+--num_classes=1000 \
+--init_lr=0.0125 \
+--load=${MODEL_DIR}/checkpoint
