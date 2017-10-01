b4e6c9efef45e1fa9f4ed6465e9b538e6b9e23c6
Debadeepta Dey <dedey@microsoft.com>
2017-09-30 18:06:32 -0700

    Added script for conveniently copying data over to Philly.

---
+++ copy_data_to_philly.sh
@@ -0,0 +1,16 @@
+
+export PHILLY_VC=msrlabs
+
+local_folder=/home/dedey/DATADRIVE1/ann_data_dir/pascal_voc_lmdb
+remote_folder_gcr=hdfs://gcr/msrlabs/dedey/ann_data_dir/
+remote_folder_cam=hdfs://cam/msrlabs/dedey/ann_data_dir/
+
+# Copy to gcr
+echo "Copying data to gcr"
+python /home/dedey/Dropbox/philly/philly_tool/tool_04_19_17_python_2.7/philly-fs.pyc -cp -r $local_folder $remote_folder_gcr
+echo "Finished copying data to gcr"
+
+# Copy to cam
+echo "Copying data to cam"
+python /home/dedey/Dropbox/philly/philly_tool/tool_04_19_17_python_2.7/philly-fs.pyc -cp -r $local_folder $remote_folder_cam
+echo "Finished copying data to cam"
