921053f07a3756580b49ca55c3a4a2270b532be1
Debadeepta Dey <dedey@microsoft.com>
2017-10-07 16:57:33 -0700

    Minor.

--- copy_code_to_philly.sh
+++ copy_code_to_philly.sh
@@ -11,6 +11,18 @@ sudo cp -r /home/dedey/AnytimeNeuralNetwork/* /mnt/msrlabs_scratch_dedey/Anytime
 # Unmount the /mnt/msrlabs_scratch_dedey folder
 sudo umount /mnt/msrlabs_scratch_dedey
 
+echo "Copying code to rr1"
+
+# Mount the msrlabs_scratch/dedey folder to /mnt/msrlabs_scratch_dedey
+sudo mount -t cifs //storage.rr1.philly.selfhost.corp.microsoft.com/msrlabs_scratch/dedey /mnt/msrlabs_scratch_dedey/ -o username=dedey,domain=REDMOND,iocharset=utf8
+
+# Copy AnytimeNeuralNetwork contents to /mnt/msrlabs_scratch_dedey/AnytimeNeuralNetwork
+sudo cp -r /home/dedey/AnytimeNeuralNetwork/* /mnt/msrlabs_scratch_dedey/AnytimeNeuralNetwork_master
+
+# Unmount the /mnt/msrlabs_scratch_dedey folder
+sudo umount /mnt/msrlabs_scratch_dedey
+
+
 echo "Copying code to cam"
 
 # Mount the msrlabs_scratch/dedey folder to /mnt/msrlabs_scratch_dedey
