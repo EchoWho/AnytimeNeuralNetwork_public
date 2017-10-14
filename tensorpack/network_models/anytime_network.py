4786c804842b8a65cedb6a38d609dec5378bf81d
hanzhang <hanzhang@andrew.cmu.edu>
2017-10-14 01:42:54 -0400

    unfix bug;;;; Fix it if fail at water fall

--- tensorpack/network_models/anytime_network.py
+++ tensorpack/network_models/anytime_network.py
@@ -1169,7 +1169,7 @@ class AnytimeLogDensenetV2(AnytimeDensenet):
 
     
     def special_filter(self, ui, x):
-        if ui == 0 or x == 0:
+        if ui == 0: # or x == 0: TODO fix this if doesn't work in first water fall
             return False
         bi = bisect.bisect_right(self.cumsum_blocks, ui)
         bi_x = bisect.bisect_right(self.cumsum_blocks, x-1)
