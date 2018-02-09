3fd5c9f973b5d41ad0fa11c09ee72f0e8033b844
Debadeepta Dey <dedey@microsoft.com>
2018-02-08 17:47:01 -0800

    Minor.

--- launch_job_rest_api.sh
+++ launch_job_rest_api.sh
@@ -19,11 +19,42 @@
 # 3207..3296
 # Num gpus : 4
 
-for i in {3207..3296}
+# for i in {3207..3296}
+# do
+# 	USERNAME="dedey"
+# 	PASSWORD="DigDug2god?"
+# 	CLUSTER="cam"
+# 	JOBSCRIPT="run_exp_$i.sh"
+# 	SPECIAL_NAME="_ann"
+# 	VC="msrlabs"
+# 	NUM_GPUS="4"
+
+# 	CMD="https://philly/api/submit?"
+# 	CMD+="buildId=0000&"
+# 	CMD+="customDockerName=custom-tf-1-1-0-python-2-7&"
+# 	CMD+="toolType=cust&"
+# 	CMD+="clusterId=$CLUSTER&"
+# 	CMD+="vcId=$VC&"
+# 	CMD+="configFile=$USERNAME%2FAnytimeNeuralNetwork_master%2F$JOBSCRIPT&"
+# 	CMD+="minGPUs=$NUM_GPUS&"
+# 	CMD+="name=cust-p-$JOBSCRIPT$SPECIAL_NAME!~!~!1&"
+# 	CMD+="isdebug=false&"
+# 	CMD+="iscrossrack=false&"
+# 	CMD+="inputDir=%2Fhdfs%2F$VC%2F$USERNAME%2Fann_data_dir%2F&"
+# 	CMD+="oneProcessPerContainer=true&"
+# 	CMD+="userName=$USERNAME"
+
+# 	curl -k --ntlm --user "$USERNAME:$PASSWORD" "$CMD"
+
+# 	echo "$CMD"
+# done
+
+# For running jobs on PhillyOnAzure
+for i in {3147..3154}
 do
 	USERNAME="dedey"
 	PASSWORD="DigDug2god?"
-	CLUSTER="cam"
+	CLUSTER="eu1"
 	JOBSCRIPT="run_exp_$i.sh"
 	SPECIAL_NAME="_ann"
 	VC="msrlabs"
@@ -42,12 +73,15 @@ do
 	CMD+="iscrossrack=false&"
 	CMD+="inputDir=%2Fhdfs%2F$VC%2F$USERNAME%2Fann_data_dir%2F&"
 	CMD+="oneProcessPerContainer=true&"
+	CMD+="rackid=p100-gpc02&"
 	CMD+="userName=$USERNAME"
+
+	echo "$CMD"
 
 	curl -k --ntlm --user "$USERNAME:$PASSWORD" "$CMD"
 
-	echo "$CMD"
 done
+
 
 
 # Exp 31
