0c1746f2475f981b6cdf94a96ef74b01a9b88e25
hanzhang <hanzhang@andrew.cmu.edu>
2018-02-15 14:31:54 -0500

    make conv2d log flops

--- tensorpack/models/conv2d.py
+++ tensorpack/models/conv2d.py
@@ -16,7 +16,7 @@ def Conv2D(x, out_channel, kernel_shape,
            padding='SAME', stride=1,
            W_init=None, b_init=None, W_mask=None,
            nl=tf.identity, split=1, use_bias=True,
-           data_format='NHWC'):
+           data_format='NHWC', log_flops=True):
     """
     2D convolution on 4D inputs.
 
@@ -67,6 +67,13 @@ def Conv2D(x, out_channel, kernel_shape,
 
     if split == 1:
         conv = tf.nn.conv2d(x, W, stride, padding, data_format=data_format)
+        if log_flops:
+            flops = 1.0 * in_channel * out_channel * kernel_shape[0] * kernel_shape[1]
+            h_dim = 1 if data_format == 'NHWC' else 2
+            w_dim = h_dim + 1
+            if in_shape[h_dim] is not None:
+                flops *= in_shape[h_dim] * in_shape[w_dim] / stride[h_dim] / stride[w_dim]
+                logger.info("conv consume FLOPS {}".format(flops))
     else:
         inputs = tf.split(x, split, channel_axis)
         kernels = tf.split(W, split, 3)
