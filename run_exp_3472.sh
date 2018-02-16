c54417980e667516a231debccc40b7e9ba4929b2
hanzhang <hanzhang@andrew.cmu.edu>
2018-02-15 21:54:35 -0500

    update 83,84; msdense count the init conv and transitions in layer idx

--- run_exp_3472.sh
+++ run_exp_3472.sh
@@ -67,4 +67,4 @@ python $CONFIG_DIR/examples/AnytimeNetwork/imagenet-msdense-ann.py \
 --log_dir=$LOG_DIR \
 --model_dir=$MODEL_DIR \
 --load=${MODEL_DIR}/checkpoint \
--f=10 --samloss=6  --msdensenet_depth=23 --batch_size=256 --nr_gpu=8 --opt_at=-1 -g=16 --num_classes=1000 --num_scales=4 --prediction_feature=msdense 
+-f=10 --samloss=6  --msdensenet_depth=23 --batch_size=256 --nr_gpu=8 --opt_at=-1 -g=16 --num_classes=1000 --num_scales=4 --prediction_feature=msdense --min_predict_unit=4 
