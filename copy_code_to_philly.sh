326839f20faea828bfe3ba731409ea0b561a0e3c
Debadeepta Dey <dedey@microsoft.com>
2018-02-16 10:46:54 -0800

    Updated passwords.

--- copy_code_to_philly.sh
+++ copy_code_to_philly.sh
@@ -3,7 +3,7 @@
 echo "Copying code to gcr"
 
 # Mount the msrlabs_scratch/dedey folder to /mnt/msrlabs_scratch_dedey
-sudo mount -t cifs //storage.gcr.philly.selfhost.corp.microsoft.com/msrlabs_scratch/dedey /mnt/msrlabs_scratch_dedey/ -o username=dedey,domain=REDMOND,iocharset=utf8,passwd="DigDug2god?"
+sudo mount -t cifs //storage.gcr.philly.selfhost.corp.microsoft.com/msrlabs_scratch/dedey /mnt/msrlabs_scratch_dedey/ -o username=dedey,domain=REDMOND,iocharset=utf8,passwd="Urdu2god?"
 
 # Copy AnytimeNeuralNetwork contents to /mnt/msrlabs_scratch_dedey/AnytimeNeuralNetwork
 sudo rsync -vr --progress --exclude='.git/' /home/dedey/AnytimeNeuralNetwork/ /mnt/msrlabs_scratch_dedey/AnytimeNeuralNetwork_master/
@@ -14,7 +14,7 @@ sudo umount /mnt/msrlabs_scratch_dedey
 echo "Copying code to rr1"
 
 # Mount the msrlabs_scratch/dedey folder to /mnt/msrlabs_scratch_dedey
-sudo mount -t cifs //storage.rr1.philly.selfhost.corp.microsoft.com/msrlabs_scratch/dedey /mnt/msrlabs_scratch_dedey/ -o username=dedey,domain=REDMOND,iocharset=utf8,passwd="DigDug2god?"
+sudo mount -t cifs //storage.rr1.philly.selfhost.corp.microsoft.com/msrlabs_scratch/dedey /mnt/msrlabs_scratch_dedey/ -o username=dedey,domain=REDMOND,iocharset=utf8,passwd="Urdu2god?"
 
 # Copy AnytimeNeuralNetwork contents to /mnt/msrlabs_scratch_dedey/AnytimeNeuralNetwork
 sudo rsync -vr --progress --exclude='.git/' /home/dedey/AnytimeNeuralNetwork/ /mnt/msrlabs_scratch_dedey/AnytimeNeuralNetwork_master/
@@ -26,7 +26,7 @@ sudo umount /mnt/msrlabs_scratch_dedey
 echo "Copying code to cam"
 
 # Mount the msrlabs_scratch/dedey folder to /mnt/msrlabs_scratch_dedey
-sudo mount -t cifs //storage.cam.philly.selfhost.corp.microsoft.com/msrlabs_scratch/dedey /mnt/msrlabs_scratch_dedey/ -o username=dedey,domain=REDMOND,iocharset=utf8,passwd="DigDug2god?"
+sudo mount -t cifs //storage.cam.philly.selfhost.corp.microsoft.com/msrlabs_scratch/dedey /mnt/msrlabs_scratch_dedey/ -o username=dedey,domain=REDMOND,iocharset=utf8,passwd="Urdu2god?"
 
 # Copy AnytimeNeuralNetwork contents to /mnt/msrlabs_scratch_dedey/AnytimeNeuralNetwork
 sudo rsync -vr --progress --exclude='.git/' /home/dedey/AnytimeNeuralNetwork/ /mnt/msrlabs_scratch_dedey/AnytimeNeuralNetwork_master/
