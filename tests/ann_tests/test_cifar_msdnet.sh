5f51182c53841f221660e504b840269a82ab7a30
hanzhang <hanzhang@cs.cmu.edu>
2018-03-31 00:17:52 -0400

    test script for ann

---
+++ tests/ann_tests/test_cifar_msdnet.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=3
+TIME=`date +"%y_%m_%d_%H_%M"`
+DATA_DIR=${GLOBAL_DATA_DIR}
+LOG_DIR=${GLOBAL_LOG_DIR}/test_cifar_msdnet/$TIME
+MODEL_DIR=${GLOBAL_MODEL_DIR}/test_cifar_msdnet/$TIME
+CONFIG_DIR=.
+
+mkdir -p $MODEL_DIR
