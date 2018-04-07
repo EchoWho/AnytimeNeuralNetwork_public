b1c88904f4f4d949ec17070c8fe12fcf91406253
Debadeepta Dey <dedey@microsoft.com>
2018-04-07 15:48:34 -0700

    Made the launch script for v2 dockers iteratable over job scripts.

--- launch_job_rest_api_v2.sh
+++ launch_job_rest_api_v2.sh
@@ -1,84 +1,80 @@
 #!/bin/bash
 
+# Exp 90 (LogDense 4 gpu)
+# 3553..3558
+# Num gpus : 4
 
-USERNAME="dedey"
-CLUSTER="gcr"
-JOBSCRIPT="run_exp_3892.sh"
-SPECIAL_NAME="_ann"
-VC="msrlabs"
-NUM_GPUS="4"
+# Exp 91 (LogDense 8 gpu)
+# 3561..3566
+# Num gpus : 8
 
-curl -H "Content-Type: application/json" \
-     -H "WWW-Authenticate: Negotiate" \
-     -H "WWW-Authenticate: NTLM" \
-     -X POST https://philly/api/v2/submit -k --ntlm -n -d \
-     '{
-        "ClusterId": "gcr",
-        "VcId": "msrlabs",
-        "JobName": "cust-p-run_exp_3892_ann",
-        "UserName": "dedey",
-        "BuildId": 0,
-        "ToolType": null,
-        "ConfigFile": "dedey/AnytimeNeuralNetwork_master/run_exp_3892.sh",
-        "Inputs": [{
-            "Name": "dataDir",
-            "Path": "/hdfs/msrlabs/dedey/ann_data_dir"
-        }],
-        "Outputs": [],
-        "IsDebug": false,
-        "CustomDockerName": "doesnotmatter",
-        "RackId": "anyConnected",
-        "MinGPUs": 4,
-        "PrevModelPath": null,
-        "ExtraParams": null,
-        "SubmitCode": "p",
-        "IsMemCheck": false,
-        "IsCrossRack": false,
-        "Registry": "phillyregistry.azurecr.io",
-        "RegistryUsername": null,
-        "RegistryPassword": null,
-        "Repository": "philly/jobs/custom/tensorflow",
-        "Tag": "tf16-py27",
-        "OneProcessPerContainer": true
-    }'
+# Exp 92 (adaloss grid search on cifar10, cifar100)
+# 3569..3784
+# Num gpus : 1
+
+# Exp 93 (baseline optimal on cifar 100 for n=9, n=17, n=25
+# 3787..3889
+# Num gpus : 1
+
+# Exp 94 (DenseNet reproduction with dropout =0.9)
+# 3892..3900
+# Num gpus : 4
+
+# Exp 95 (Copy of 94 on 8 gpus)
+# 3903..3911
+# Num gpus : 8
+
+# Exp 96 (ResNext with adaloss)
+# 3914..3925
+# Num gpus : 4
+
+# Exp 97 (Copy of 96 w/ 8 gpus)
+# 3928..3939
+# Num gpus : 8
+
+for i in {3892..3900}
+do
+    USERNAME="dedey"
+    CLUSTER="gcr"
+    JOBSCRIPT="run_exp_$i.sh"
+    SPECIAL_NAME="_ann1"
+    VC="msrlabs"
+    NUM_GPUS="4"
+
+    curl -H "Content-Type: application/json" \
+         -H "WWW-Authenticate: Negotiate" \
+         -H "WWW-Authenticate: NTLM" \
+         -X POST https://philly/api/v2/submit -k --ntlm -n -d \
+         '{
+            "ClusterId": "'${CLUSTER}'",
+            "VcId": "'${VC}'",
+            "JobName": "cust-p-'${JOBSCRIPT}''${SPECIAL_NAME}'",
+            "UserName": "'${USERNAME}'",
+            "BuildId": 0,
+            "ToolType": null,
+            "ConfigFile": "'${USERNAME}'/AnytimeNeuralNetwork_master/'${JOBSCRIPT}'",
+            "Inputs": [{
+                "Name": "dataDir",
+                "Path": "/hdfs/'${VC}'/dedey/ann_data_dir"
+            }],
+            "Outputs": [],
+            "IsDebug": false,
+            "CustomDockerName": "doesnotmatter",
+            "RackId": "anyConnected",
+            "MinGPUs": '${NUM_GPUS}',
+            "PrevModelPath": null,
+            "ExtraParams": null,
+            "SubmitCode": "p",
+            "IsMemCheck": false,
+            "IsCrossRack": false,
+            "Registry": "phillyregistry.azurecr.io",
+            "RegistryUsername": null,
+            "RegistryPassword": null,
+            "Repository": "philly/jobs/custom/tensorflow",
+            "Tag": "tf16-py27",
+            "OneProcessPerContainer": true
+        }'
+done
 
 
 
-
-# generate_post_data()
-# {
-#   cat <<EOF
-# {
-#   "account": {
-#     "email": "$email",
-#     "screenName": "$screenName",
-#     "type": "$theType",
-#     "passwordSettings": {
-#       "password": "$password",
-#       "passwordConfirm": "$password"
-#     }
-#   },
-#   "firstName": "$firstName",
-#   "lastName": "$lastName",
-#   "middleName": "$middleName",
-#   "locale": "$locale",
-#   "registrationSiteId": "$registrationSiteId",
-#   "receiveEmail": "$receiveEmail",
-#   "dateOfBirth": "$dob",
-#   "mobileNumber": "$mobileNumber",
-#   "gender": "$gender",
-#   "fuelActivationDate": "$fuelActivationDate",
-#   "postalCode": "$postalCode",
-#   "country": "$country",
-#   "city": "$city",
-#   "state": "$state",
-#   "bio": "$bio",
-#   "jpFirstNameKana": "$jpFirstNameKana",
-#   "jpLastNameKana": "$jpLastNameKana",
-#   "height": "$height",
-#   "weight": "$weight",
-#   "distanceUnit": "MILES",
-#   "weightUnit": "POUNDS",
-#   "heightUnit": "FT/INCHES"
-# }
-# EOF 
