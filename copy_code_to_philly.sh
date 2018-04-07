902c1516e0b833df3303696de4d0026e22cda4d7
Debadeepta Dey <dedey@microsoft.com>
2018-04-07 15:18:35 -0700

    Fixed the launch script to actually work with V2 custom docker jobs.

--- copy_code_to_philly.sh
+++ copy_code_to_philly.sh
@@ -25,11 +25,11 @@ sudo umount /mnt/msrlabs_scratch_dedey
 
 # echo "Copying code to cam"
 
-# # Mount the msrlabs_scratch/dedey folder to /mnt/msrlabs_scratch_dedey
-# sudo mount -t cifs //storage.cam.philly.selfhost.corp.microsoft.com/msrlabs_scratch/dedey /mnt/msrlabs_scratch_dedey/ -o username=dedey,domain=REDMOND,iocharset=utf8,passwd="Urdu2god?"
+# Mount the msrlabs_scratch/dedey folder to /mnt/msrlabs_scratch_dedey
+sudo mount -t cifs //storage.cam.philly.selfhost.corp.microsoft.com/msrlabs_scratch/dedey /mnt/msrlabs_scratch_dedey/ -o username=dedey,domain=REDMOND,iocharset=utf8,passwd="Urdu2god?"
 
-# # Copy AnytimeNeuralNetwork contents to /mnt/msrlabs_scratch_dedey/AnytimeNeuralNetwork
-# sudo rsync -vr --progress --exclude='.git/' /home/dedey/AnytimeNeuralNetwork/ /mnt/msrlabs_scratch_dedey/AnytimeNeuralNetwork_master/
+# Copy AnytimeNeuralNetwork contents to /mnt/msrlabs_scratch_dedey/AnytimeNeuralNetwork
+sudo rsync -vr --progress --exclude='.git/' /home/dedey/AnytimeNeuralNetwork/ /mnt/msrlabs_scratch_dedey/AnytimeNeuralNetwork_master/
 
-# # Unmount the /mnt/msrlabs_scratch_dedey folder
-# sudo umount /mnt/msrlabs_scratch_dedey
+# Unmount the /mnt/msrlabs_scratch_dedey folder
+sudo umount /mnt/msrlabs_scratch_dedey
