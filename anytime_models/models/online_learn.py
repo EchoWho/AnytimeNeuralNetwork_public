47169e5de160c620b0f2bca499d53e8179e34846
hanzhang <hanzhang@cs.cmu.edu>
2018-04-24 16:54:13 -0400

    change adaloss so that the orig max has weight 1

--- anytime_models/models/online_learn.py
+++ anytime_models/models/online_learn.py
@@ -349,7 +349,7 @@ class AdaptiveLossWeight(Callback):
             self._weight = 1. / self.avg_losses + 1e-8
             self._weight /= np.max(self._weight)
             self._weight[-1] += self.final_extra
-            self._weight /= np.max(self._weight)
+            #self._weight /= np.max(self._weight)
             self._weight = self._weight * (1-self.gamma) \
                     + np.ones(self.K, dtype=np.float32) * self.gamma
             self.assign_op.eval(feed_dict={self.weight_holder : self._weight})
