0f636e57d29f4f5f8ac70a0033b6e28e6f6a604e
hanzhang <hanzhang@cs.cmu.edu>
2018-04-23 21:24:58 -0400

    exp 110

--- launch_job_rest_api_v2_azure.sh
+++ launch_job_rest_api_v2_azure.sh
@@ -81,7 +81,11 @@
 # 4024..4068
 # Num gpus : 1
 
-for i in {3996..4007} # 4 gpu ; {4010..4021} # 8 gpu ; {4024..4068} # 1 gpu
+# Exp 110 (4 gpu resnet with smaller number of prediction and better anytime predictors)
+# 4071..4078
+# Num gpus : 4
+
+for i in {3996..4007} # 4 gpu ; {4010..4021} # 8 gpu ; {4024..4068} # 1 gpu  ; {4071..4078} # 4gpu
 do
     USERNAME="dedey"
     CLUSTER="eu1"
