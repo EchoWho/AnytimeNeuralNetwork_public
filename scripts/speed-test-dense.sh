416dc0fd46076b4aa092212083f1ffface17aa98
hanzhang <hanzhang@cs.cmu.edu>
2018-04-26 00:22:16 -0400

    speed test script and code for doing so

--- scripts/speed-test-dense.sh
+++ scripts/speed-test-dense.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=3
-# Note that $s need to be adjusted as well. (d, s):
-# (14, 1), (26, 2), (50, 2), (101, 3), (18, 2), (34, 2), (152, 5)
 
 for trial in `seq 5`
 do
-for dns in 14,1,4 26,2,4 50,2,8 101,3,11
+for dns in 121,9,6 169,14,6
 do
 	d=`echo $dns | grep "^[0-9]*," -o | grep "[0-9]*" -o`
 	s=`echo $dns | grep ",[0-9]*," -o | grep "[0-9]*" -o`
@@ -15,8 +13,8 @@ do
 	do
 
 		DATA_DIR=${GLOBAL_DATA_DIR}
-		model_dn=imagenet-resnet-ann-d${d}-s${s}-np${np}-trial${trial}
-		LOG_DIR=${GLOBAL_LOG_DIR}/resnet-speed-test/${model_dn}
+		model_dn=imagenet-dense-ann-d${d}-s${s}-np${np}-trial${trial}
+		LOG_DIR=${GLOBAL_LOG_DIR}/densenet-speed-test/${model_dn}
 		MODEL_DIR=${GLOBAL_MODEL_DIR}/imagenet_model/${model_dn}
 		CONFIG_DIR=.
 
@@ -25,14 +23,17 @@ do
 		echo $model_dn
 
 		# Run the actual job
-		python $CONFIG_DIR/anytime_models/examples/imagenet-ann.py \
+		python $CONFIG_DIR/anytime_models/examples/imagenet-dense-ann.py \
 		--data_dir=$DATA_DIR \
 		--log_dir=$LOG_DIR \
 		--model_dir=$MODEL_DIR \
-		-d=${d} -s=${s} --num_anytime_preds=${np} \
+		--num_anytime_preds=${np} \
+		--densenet_depth=${d} -s=${s} \
+		--exp_gamma=0.07 --sum_rand_ratio=0 --is_select_arr \
+		-f=5 --samloss=100  \
 		--batch_size=64 --nr_gpu=1 \
-		-f=5 --samloss=100 --exp_gamma=0.07 --sum_rand_ratio=0 --is_select_arr \
-		--opt_at=-1 -c=64 --num_classes=1000 \
+		--densenet_version=dense --min_predict_unit=10 \
+		--reduction_ratio=0.5 --opt_at=-1 -g=32 --num_classes=1000 \
 		--evaluate=val
 	done
 done
