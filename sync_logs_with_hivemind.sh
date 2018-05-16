16f5df5b70c68d6b14fae3ca0e80c7bde883fb78
Debadeepta Dey <dedey@microsoft.com>
2018-05-16 10:16:57 -0700

    Purged philly related files.

--- sync_logs_with_hivemind.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-sudo find /home/dedey/DATADRIVE1/ann_models_logs/  -name events.out.tfevents*  -exec rm -f {} \;
-rsync -r -v -e ssh /home/dedey/DATADRIVE1/ann_models_logs/ debadeepta@hivemind.ml.cmu.edu:/data2/saved_models_ann/ann_models_logs/
